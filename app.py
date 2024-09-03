477d9e38c39c8a106b017aa7a5ebb9d91dbb621a
Sudheerbmb <sudheermsdvk@gmail.com>
2024-09-03 18:00:16 +0530

    Finished

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask,render_template
 app= Flask(__name__)
 
 @app.route("/")
-def start():
-    return "Starting"
-
-@app.route("/mbsa")
 def mbsa():
     return render_template("index.html")
+
+
 
 
 
